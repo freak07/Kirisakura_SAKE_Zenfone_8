b375d0aa1892032151f9eafb59873244288cda70
Cosmin Tanislav <demonsingur@gmail.com>
2022-03-05 14:14:52 +0200

    input: touchscreen: focaltech_touch_v3_3: wait 1s before suspending
    Change-Id: I5b21b7a4f9075b87ee294be068353ad90a6a77f7
    (cherry picked from commit d7499eaeda9017ee8a408a8436dd19fd9c699977)

--- drivers/input/touchscreen/focaltech_touch_v3_3/focaltech_core.c
+++ drivers/input/touchscreen/focaltech_touch_v3_3/focaltech_core.c
@@ -1612,8 +1612,9 @@ static void fts_resume_work(struct work_struct *work)
 
 static void fts_suspend_work(struct work_struct *work)
 {
+	struct delayed_work *dwork = to_delayed_work(work);
 	struct fts_ts_data *ts_data =
-		container_of(work, struct fts_ts_data, suspend_work);
+		container_of(dwork, struct fts_ts_data, suspend_work);
 
 	fts_ts_suspend(ts_data->dev);
 }
@@ -1640,6 +1641,7 @@ static int fb_notifier_callback(struct notifier_block *self,
 		if (event == DRM_PANEL_EVENT_BLANK) {
 			FTS_DEBUG("resume: event = %lu, not care\n", event);
 		} else if (event == DRM_PANEL_EARLY_EVENT_BLANK) {
+			cancel_delayed_work(&fts_data->suspend_work);
 			queue_work(fts_data->ts_workqueue,
 				   &fts_data->resume_work);
 		}
@@ -1647,15 +1649,18 @@ static int fb_notifier_callback(struct notifier_block *self,
 
 	case DRM_PANEL_BLANK_POWERDOWN:
 		if (event == DRM_PANEL_EARLY_EVENT_BLANK) {
-			queue_work(fts_data->ts_workqueue,
-				   &fts_data->suspend_work);
+			mod_delayed_work(fts_data->ts_workqueue,
+					 &fts_data->suspend_work,
+					 msecs_to_jiffies(1000));
 		} else if (event == DRM_PANEL_EVENT_BLANK) {
 			FTS_DEBUG("suspend: event = %lu, not care\n", event);
 		}
 		break;
 
 	case DRM_PANEL_BLANK_LP:
-		queue_work(fts_data->ts_workqueue, &fts_data->suspend_work);
+		mod_delayed_work(fts_data->ts_workqueue,
+				 &fts_data->suspend_work,
+				 msecs_to_jiffies(1000));
 		break;
 	default:
 		FTS_DEBUG("FB BLANK(%d) do not need process\n", *blank);
@@ -1823,7 +1828,7 @@ static int fts_ts_probe_entry(struct fts_ts_data *ts_data)
 #if defined(CONFIG_DRM) && defined(CONFIG_DRM_PANEL)
 	if (ts_data->ts_workqueue) {
 		INIT_WORK(&ts_data->resume_work, fts_resume_work);
-		INIT_WORK(&ts_data->suspend_work, fts_suspend_work);
+		INIT_DELAYED_WORK(&ts_data->suspend_work, fts_suspend_work);
 	}
 
 	ts_data->fb_notif.notifier_call = fb_notifier_callback;
